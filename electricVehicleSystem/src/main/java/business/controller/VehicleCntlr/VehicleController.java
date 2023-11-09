ad62d3453c172eb95a7e95a767638ddc55ea4843
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2023-11-08 23:06:52 -0500

    onepage show complete and some cleanup

--- electricVehicleSystem/src/main/java/business/controller/VehicleCntlr/VehicleController.java
+++ electricVehicleSystem/src/main/java/business/controller/VehicleCntlr/VehicleController.java
@@ -13,26 +13,24 @@ import presistence.daoLayer.catalogDAO;
 public class VehicleController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public VehicleController() {
-    }
+	public VehicleController() {
+	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-        ServletContext context = getServletContext();
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		catalogDAO itemDAO = new catalogDAO();
-		
-	    String id = request.getParameter("id");
-		Item item = itemDAO.getVehicleById(id);
-        request.setAttribute("item", item);
 
-        // Forward the request to the JSP page that will display the item details
-        request.getRequestDispatcher("/index/oneItem.jspx").forward(request, response);
+		String id = request.getParameter("id");
+		Item item = itemDAO.getVehicleById(id);
+		request.setAttribute("item", item);
+
+		// Forward the request to the JSP page that will display the item details
+		request.getRequestDispatcher("/index/oneItem.jspx").forward(request, response);
 
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
---
+++ electricVehicleSystem/src/main/webapp/scripts/populate.js
@@ -0,0 +1,4 @@
+/**
+ * 
+ */
+console.log("IM HERE")
